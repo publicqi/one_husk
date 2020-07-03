a67d942e28d86afc8139d11534a7272c6179a087
publicqi <qisu@ucsb.edu>
2020-07-03 16:54:05 +0800

    Change how fields are stored

--- one_husk/__init__.py
+++ one_husk/__init__.py
@@ -1,6 +1,5 @@
 import struct
 
-
 def p64(x):
     return struct.pack('<Q', x)
 
@@ -13,40 +12,15 @@ class fakeFile(object):
     # Most variable names are inherited from
     # https://github.com/Gallopsled/pwntools/blob/cc6d272760/pwnlib/filepointer.py#L94-L338
 
-    vars_ = []
-    length = {}
+    vars_ = {}
+    fields = ['flags', '_IO_read_ptr', '_IO_read_end', '_IO_read_base', '_IO_write_base', '_IO_write_ptr', '_IO_write_end', '_IO_buf_base', '_IO_buf_end', '_IO_save_base', '_IO_backup_base', '_IO_save_end', 'markers', 'chain', 'fileno', '_flags2', '_old_offset', '_cur_column', '_vtable_offset', '_shortbuf', 'unknown1', '_lock', '_offset', '_codecvt', '_wide_data', 'unknown2', 'vtable', '_allocate_buffer', '_free_buffer']
 
     def __init__(self):
         self.setdefault()
 
     def setdefault(self):
-        self.flags = 0
-        self._IO_read_ptr = 0
-        self._IO_read_end = 0
-        self._IO_read_base = 0
-        self._IO_write_base = 0
-        self._IO_write_ptr = 0
-        self._IO_write_end = 0
-        self._IO_buf_base = 0
-        self._IO_buf_end = 0
-        self._IO_save_base = 0
-        self._IO_backup_base = 0
-        self._IO_save_end = 0
-        self.markers = 0
-        self.chain = 0
-        self.fileno = 0
-        self._flags2 = 0
-        self._old_offset = 0
-        self._cur_column = 0
-        self._vtable_offset = 0
-        self._shortbuf = 0
-        self.unknown1 = 0
-        self._lock = 0
-        self._offset = 0
-        self._codecvt = 0
-        self._wide_data = 0
-        self.unknown2 = 0
-        self.vtable = 0
+        for field in self.fields:
+            self.vars_[field] = 0
 
-        self._allocate_buffer = 0
-        self._free_buffer = 0
+    def build(self):
+        return ''.join([p64(self.vars_[field]) for field in self.fields])
